3c442314b587dd0e44015a4fee06bea58cb92fec
maywebdv <167105867+maywebdv@users.noreply.github.com>
2024-07-29 21:27:03 +0100

    NewVersion

--- src/components/admin/articles/Listarticles.jsx
+++ src/components/admin/articles/Listarticles.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { getArticles } from '../../../features/articleSlice'
+import { useDispatch } from 'react-redux'
+import Affichearticle from './Affichearticle';
 const Listarticles = () => {
+ const dispatch=useDispatch()
+
+ const loadarticles=async()=>{
+  await dispatch(getArticles())
+ }
+ useEffect(()=>{
+  loadarticles()
+ },[dispatch]) 
+
   return (
     <div>
-      Liste des articles
+   <Affichearticle/>
     </div>
   )
 }
